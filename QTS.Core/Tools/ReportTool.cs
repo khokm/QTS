bed6601d20c22cd894d34941534cb0606e95fb99
Maxim Khokhryakov <khokm@ya.ru>
2018-09-16 13:41:28 +0500

    fix

--- QTS.Core/Tools/ReportTool.cs
+++ QTS.Core/Tools/ReportTool.cs
@@ -6,7 +6,7 @@ namespace QTS.Core.Tools
     static class ReportTool
     {
         public static string MakeReport(DiagramData diagram, IEnumerable<Metric> metrics) =>
-            "Анализ диаграммы:" + string.Join("\r\n", metrics.Select(metric => string.Format("{0}: {1} {2}", metric.Name, metric.Formula(diagram), metric.Units)));
+            "Анализ диаграммы:\r\n" + string.Join("\r\n", metrics.Select(metric => string.Format("{0}: {1} {2}", metric.Name, metric.Formula(diagram), metric.Units)));
 
         public static IEnumerable<T> CreateGraphs<T>(IEnumerable<DiagramData> diagrams, IEnumerable<Metric> metrics, QueuePlaceGradientData gradient, IGraphicsFactory<InteractiveDiagram, T> factory) where T : IGraph => 
             metrics.Select(metric => 
